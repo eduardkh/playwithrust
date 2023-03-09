7c0e027fe72f8d2d245d51c32bfec4f519c821aa
Eduard Khiaev <eddietheone1984@gmail.com>
2023-03-09 20:33:05 +0200

    string operations

--- strings/src/main.rs
+++ strings/src/main.rs
@@ -8,4 +8,16 @@ fn main() {
     vstring.push_str("string");
     vstring.push('s');
     println!("{}", vstring);
+
+    // string operations
+    println!("vstring empty: {}", vstring.is_empty());
+    println!("vstring len: {}", vstring.len());
+    println!("vstring byte capacity: {}", vstring.capacity());
+    println!("vstring contains 'rings': {}", vstring.contains("rings"));
+    // multiple string operations
+    vstring.push_str("    ");
+    println!(
+        "vstring after adding spaces, trim and len: {}",
+        vstring.trim().len()
+    );
 }
